b419824c5aee8e6639649565c18b49c42b536e72
Hansa Anuradha <hansaanuradha@Hansas-MacBook-Pro.local>
2018-12-21 18:19:49 +0530

    Send http request, get the response as JSON, Parse JSON then Update UI

--- Cilmate/Controller/WeatherViewController.swift
+++ Cilmate/Controller/WeatherViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreLocation
+import Alamofire
+import SwiftyJSON
 
 class WeatherViewController: UIViewController, CLLocationManagerDelegate {
 
@@ -17,6 +19,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     // Declare instance variables
     let locationManager = CLLocationManager()
+    let weatherDataModel = WeatherDataModel()
     
     // IBOutlets
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -35,7 +38,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     //  Location Manager Delegate Methods
-    /***********************************************************************************/
+    /*********************************************************************************************************************************************/
     
     // didUpdateLocations method - Tells the deligate that new location data is available
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -51,6 +54,9 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
             
             // Create a dictionaty with latitude, longitude and app id
             let params : [String : String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+            
+            // Get weather data from the Web Server
+            getWeatherData(url : WEATHER_URL, parameters : params)
         }
     }
     
@@ -60,7 +66,72 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         print(error)
         cityLabel.text = "Location Unavailable" // Tells the User that the location is unavailable
     }
+    
+    //  Networking
+    /*********************************************************************************************************************************************/
 
+    // getWeather method - Fetch weather infromation from the web service
+    func getWeatherData(url : String, parameters : [String : String]) {
+        
+        // Alamorefire requests data from the server in the backgroud (Asyncronous), once the background process is completed, we will have a response
+        Alamofire.request(url, method : .get, parameters: parameters).responseJSON {
+            response in // Now we are trying to tackle with the respond which is a JSON the we have got
+            if response.result.isSuccess { // Response is successfull
+                print("Success! Got the weather data!") // Print that response is successful
+                
+                let weatherJSON : JSON = JSON(response.result.value!) // Get the data in response to a JSON / We have force unwrapped the value, since it has been checked and confirmed that it has a value for sure
+                print(weatherJSON) // Print the JSON
+                // Parse JSON
+                self.updateWeatherData(json : weatherJSON)
+                
+                
+                
+            } else { // Responce is failure
+                print("Error \(String(describing: response.result.error))") // Print the error
+                self.cityLabel.text = "Connection Issues" // Show the User the error
+            }
+        }
+    }
+    
+    
+    //  JSON Parsing
+    /*********************************************************************************************************************************************/
+    
+    // updateWeatherData method - Parse JSON data then update UI
+    func updateWeatherData(json : JSON) {
+        
+        // Do an Optional binding to be sure this (json["main"]["temp"]) statement has some value (Not a nil value)
+        if let temperatureResult = json["main"]["temp"].double { // Capture the temperature data from the json and convert into Double from type JSON
+        
+        weatherDataModel.temperature = Int(temperatureResult - 273.15) // Convert the temperature into Celsius and convert it into Integer, then store it in weatherDataModel object
+        
+        weatherDataModel.city = json["name"].stringValue // Capture the name from the json, convert it into String from type JSON, then store it in weatherDataModel object
+        
+        weatherDataModel.condition = json["weather"][0]["id"].intValue
+        
+        weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition) // Store the weather condition icon name in weatherDataModel object
+            
+            // Update the UI with weather data
+            updateUIWithWeatherData()
+            
+        } else {
+            cityLabel.text = "Weather Unavailable"
+        }
+        
+    }
+    
+    
+    //  UI Update
+    /*********************************************************************************************************************************************/
 
+    // updateUIWithWeatherData method - Update the UI with weather data
+    func updateUIWithWeatherData() {
+        
+        cityLabel.text = weatherDataModel.city // Update the city label
+        temperatureLabel.text = "\(weatherDataModel.temperature)°" // Update the temperature label
+        weatherIcon.image = UIImage(named: weatherDataModel.weatherIconName) // Update the weather icon
+        
+    }
+    
 }
 
